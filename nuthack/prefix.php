9464b2719e6aa3bcdacdefc3ec128677a6b8b1fd
Philburridge <burridge@live.se>
2015-11-27 16:00:17 +0100

    delete duplicated files

--- nuthack/prefix.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set('display_errors','1');
-ini_set('display_startup_errors','1');
-error_reporting (E_ALL);
-// Sätter på buffering så inget output skickas utan lagras internt
-ob_start()
-?>
