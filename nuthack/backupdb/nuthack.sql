df4b77f409e79186a8a66722728b4ae72b119082
Emma Edvardsson <emmed608@student.liu.se>
2015-12-03 10:57:03 +0100

    new db backup

--- nuthack/backupdb/nuthack.sql
+++ nuthack/backupdb/nuthack.sql
@@ -1,154 +1,118 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 26, 2015 at 04:24 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Värd: localhost
+-- Skapad: 03 dec 2015 kl 04:55
+-- Serverversion: 5.5.46-0ubuntu0.14.04.2
+-- PHP-version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Database: `nuthack`
+-- Databas: `nuthack`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Activity`
+-- Tabellstruktur `Activity`
 --
 
-CREATE TABLE `Activity` (
+CREATE TABLE IF NOT EXISTS `Activity` (
   `userId` int(6) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `text` text NOT NULL,
-  `place` varchar(150) NOT NULL,
-  `activityType` varchar(10) NOT NULL,
-`activityId` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `date` date NOT NULL,
+  `text` text COLLATE utf8_swedish_ci NOT NULL,
+  `place` varchar(150) COLLATE utf8_swedish_ci NOT NULL,
+  `activityType` varchar(10) COLLATE utf8_swedish_ci NOT NULL,
+  `activityId` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`activityId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=145 ;
 
 --
--- Dumping data for table `Activity`
+-- Dumpning av Data i tabell `Activity`
 --
 
 INSERT INTO `Activity` (`userId`, `date`, `text`, `place`, `activityType`, `activityId`) VALUES
-(1, '2015-11-25 14:56:15', 'Bra springtur', 'Vrinneviskogen, Norrköping', 'Running', 1),
-(1, '2015-11-24 15:46:00', 'Bra gympass', 'WorldClass Pronova, Norrköping', 'Gym', 2),
-(1, '2015-11-25 14:59:45', 'Simmade sjukt långt', 'Medley, Norrköping', 'Swimming', 3);
+(1, '2015-12-01', 'Bra gympass', 'WorldClass Pronova, NorrkÃ¶ping', 'Gym', 2),
+(1, '2015-11-25', 'Simmade sjuukt lÃ¥ngt', 'Medley, NorrkÃ¶ping', 'Swimming', 3),
+(1, '2015-11-30', 'Maxtempo', 'StrÃ¶mmen, NorrkÃ¶ping', 'Running', 116),
+(1, '2015-12-02', 'MilspÃ¥ret. ', 'Vrinneviskogen, NorrkÃ¶ping', 'Running', 144);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Gym`
+-- Tabellstruktur `Gym`
 --
 
-CREATE TABLE `Gym` (
+CREATE TABLE IF NOT EXISTS `Gym` (
   `activityId` int(11) NOT NULL,
-  `exercise` varchar(100) NOT NULL,
+  `exercise` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
   `weight` int(11) NOT NULL,
   `repetitions` int(11) NOT NULL,
-`gymId` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `gymId` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`gymId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=12 ;
 
 --
--- Dumping data for table `Gym`
+-- Dumpning av Data i tabell `Gym`
 --
 
 INSERT INTO `Gym` (`activityId`, `exercise`, `weight`, `repetitions`, `gymId`) VALUES
-(1, 'DL', 140, 2, 1),
-(2, 'C and J', 79, 5, 2),
-(3, 'PC', 90, 3, 3);
+(2, 'C and J', 89, 5, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Running`
+-- Tabellstruktur `Running`
 --
 
-CREATE TABLE `Running` (
+CREATE TABLE IF NOT EXISTS `Running` (
   `activityId` int(11) NOT NULL,
   `length` int(11) NOT NULL,
   `time` time NOT NULL,
-`runningId` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `runningId` int(11) NOT NULL AUTO_INCREMENT,
+  UNIQUE KEY `runningId` (`runningId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=55 ;
 
 --
--- Dumping data for table `Running`
+-- Dumpning av Data i tabell `Running`
 --
 
 INSERT INTO `Running` (`activityId`, `length`, `time`, `runningId`) VALUES
-(1, 5000, '23:02:12', 1),
-(2, 5000, '23:02:18', 5),
-(3, 5000, '23:23:12', 6);
+(116, 5000, '00:19:46', 30),
+(144, 10000, '00:58:23', 54);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Swimming`
+-- Tabellstruktur `Swimming`
 --
 
-CREATE TABLE `Swimming` (
-  `stroke` varchar(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Swimming` (
+  `stroke` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
   `length` int(11) NOT NULL,
   `activityId` int(11) NOT NULL,
-  `time` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `time` time NOT NULL,
+  `swimmingId` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`swimmingId`),
+  UNIQUE KEY `activityId` (`activityId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `Swimming`
+-- Dumpning av Data i tabell `Swimming`
 --
 
-INSERT INTO `Swimming` (`stroke`, `length`, `activityId`, `time`) VALUES
-('Fr', 50, 1, '00:00:00'),
-('Fj', 200, 2, '00:00:00'),
-('Ry', 100, 3, '00:00:00');
+INSERT INTO `Swimming` (`stroke`, `length`, `activityId`, `time`, `swimmingId`) VALUES
+('Ry', 123, 3, '00:58:06', 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Activity`
---
-ALTER TABLE `Activity`
- ADD PRIMARY KEY (`activityId`);
-
---
--- Indexes for table `Gym`
---
-ALTER TABLE `Gym`
- ADD PRIMARY KEY (`gymId`);
-
---
--- Indexes for table `Running`
---
-ALTER TABLE `Running`
- ADD UNIQUE KEY `runningId` (`runningId`);
-
---
--- Indexes for table `Swimming`
---
-ALTER TABLE `Swimming`
- ADD UNIQUE KEY `activityId` (`activityId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Activity`
---
-ALTER TABLE `Activity`
-MODIFY `activityId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `Gym`
---
-ALTER TABLE `Gym`
-MODIFY `gymId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `Running`
---
-ALTER TABLE `Running`
-MODIFY `runningId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
